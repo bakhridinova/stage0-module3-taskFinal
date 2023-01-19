5c3bbcacd5ded3c8c82eb94e9b647477965c7519
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-19 22:20:23 +0500

    updated DigitsSumCalculator.java, DivisionByZero.java, NumberReverter.java, NumbersPrinter.java, PrintingFalse.java, TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sumOfDigits = 0;
+        for (int i = 1; i < 5; i++) {
+            sumOfDigits += number % 10;
+            number /= 10;
+        }
+        System.out.println(sumOfDigits);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reverted = 0;
+        for (int i = 1; i < 4; i++) {
+               reverted += (int) Math.pow(10, 3 - i) * (number % 10);
+               number /= 10;
+        }
+        System.out.println(reverted);
     }
 }
